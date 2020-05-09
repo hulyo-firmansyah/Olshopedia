6ae62cea59536ba4bd8e2ff6f746c4155f66d45a
NickmanUiop <kocakmantab@gmail.com>
2020-05-09 10:43:23 +0700

    test index cork

--- src/resources/views/depan/cork/index.blade.php
+++ src/resources/views/depan/cork/index.blade.php
@@ -149,22 +149,12 @@
             </ul>
             <ul class="navbar-nav flex-row ml-auto ">
                 <li class="nav-item more-dropdown">
-                    <div class="dropdown  custom-dropdown-icon">
-                        <a class="dropdown-toggle btn" href="#" role="button" id="customDropdown" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false"><span>Settings</span> <svg
-                                xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                                stroke-linejoin="round" class="feather feather-chevron-down">
-                                <polyline points="6 9 12 15 18 9"></polyline>
-                            </svg></a>
-
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="customDropdown">
-                            <a class="dropdown-item" data-value="Settings" href="javascript:void(0);">Settings</a>
-                            <a class="dropdown-item" data-value="Mail" href="javascript:void(0);">Mail</a>
-                            <a class="dropdown-item" data-value="Print" href="javascript:void(0);">Print</a>
-                            <a class="dropdown-item" data-value="Download" href="javascript:void(0);">Download</a>
-                            <a class="dropdown-item" data-value="Share" href="javascript:void(0);">Share</a>
-                        </div>
+                    <div class="custom-dropdown-icon">
+                        <button type='button' class='btn btn-outline-dark mt-1'>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-shopping-cart"><circle cx="9" cy="21" r="1"></circle><circle cx="20" cy="21" r="1"></circle><path d="M1 1h4l2.68 13.39a2 2 0 0 0 2 1.61h9.72a2 2 0 0 0 2-1.61L23 6H6"></path>
+                            </svg>
+                            Cart
+                        </button>
                     </div>
                 </li>
             </ul>
@@ -216,13 +206,7 @@
                         rights reserved.</p>
                 </div>
                 <div class="footer-section f-section-2">
-                    <p class="">Coded with <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                            viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
-                            stroke-linecap="round" stroke-linejoin="round" class="feather feather-heart">
-                            <path
-                                d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z">
-                            </path>
-                        </svg></p>
+                    <p class="">Olshopedia</p>
                 </div>
             </div>
         </div>
