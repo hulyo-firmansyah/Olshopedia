78bc09bc0ed8cacfa6959472beea7532d551d1d2
NickmanUiop <kocakmantab@gmail.com>
2020-06-16 09:56:57 +0700

    penambahan table bukti pembayaran

---
+++ src/database/migrations/2020_06_16_094236_create_t_konfirmasi_bayar_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTKonfirmasiBayarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('t_konfirmasi_bayar', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->text('atas_nama');
+            $table->unsignedInteger('nominal');
+            $table->text('bank_tujuan');
+            $table->dateTime('tgl_transfer');
+            $table->unsignedBigInteger('foto_id');
+            $table->text('catatan')->nullable();
+            $table->unsignedBigInteger('data_of');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('t_konfirmasi_bayar');
+    }
+}
